bd23f5555e797bea2ae3080734fc01ff1549d035
JasonYES <fyCode@163.com>
2019-03-05 10:23:05 +0800

    map finally appear.

--- src/components/charts/map.vue
+++ src/components/charts/map.vue
@@ -3,11 +3,12 @@
 </template>
 <script>
 import echarts from "echarts";
+import "echarts/map/js/china.js";
 import tdTheme from "./theme.json";
 import { on, off } from "@/libs/tools";
 // echarts.registerTheme("tdTheme", tdTheme);
 export default {
-  name: "ChartScatter",
+  name: "ChartMap",
   props: {
     value: Array, // 值
     text: String, // 标题
@@ -25,44 +26,93 @@ export default {
   },
   mounted() {
     this.$nextTick(() => {
-      let keys = Object.keys(this.value[0]);
-      let xAxisName = keys[1]; //定义X轴名字
-      let yAxisName = keys[2]; //定义y轴名字
       let option = {
-        dataset: {
-          // dimensions: ["count", "score"],
-          source: this.value
-        },
-        tooltip: {},
         title: {
-          text: this.text,
-          subtext: this.subtext,
+          text: "iphone销量",
+          subtext: "纯属虚构",
           x: "center"
         },
-        xAxis: {
-          type: "value",
-          name: xAxisName,
-          nameTextStyle: {
-            color: "#242424",
-            fontSize: 14
+        tooltip: {
+          trigger: "item"
+        },
+        legend: {
+          orient: "vertical",
+          x: "left",
+          data: ["iphoneX"]
+        },
+        dataRange: {
+          min: 0,
+          max: 2500,
+          x: "left",
+          y: "bottom",
+          text: ["高", "低"],
+          calculable: true
+        },
+        toolbox: {
+          show: true,
+          orient: "vertical",
+          x: "right",
+          y: "center",
+          feature: {
+            mark: { show: true },
+            dataView: { show: true, readOnly: false },
+            restore: { show: true },
+            saveAsImage: { show: true }
           }
         },
-        yAxis: {
-          type: "value",
-          name: yAxisName,
-          nameTextStyle: {
-            color: "#242424",
-            fontSize: 14
+        roamController: {
+          show: true,
+          x: "right",
+          mapTypeControl: {
+            china: true
           }
         },
         series: [
           {
-            type: "scatter",
-            encode: {
-              tooltip: [0, 1, 2],
-              x: 1, // 默认第一位和第二位对应坐标轴
-              y: 2 // 默认第一位和第二位对应坐标轴
-            }
+            name: "iphoneX",
+            type: "map",
+            mapType: "china",
+            roam: false,
+            itemStyle: {
+              normal: { label: { show: true } },
+              emphasis: { label: { show: true } }
+            },
+            data: [
+              { name: "北京", value: Math.round(Math.random() * 1000) },
+              { name: "天津", value: Math.round(Math.random() * 1000) },
+              { name: "上海", value: Math.round(Math.random() * 1000) },
+              { name: "重庆", value: Math.round(Math.random() * 1000) },
+              { name: "河北", value: Math.round(Math.random() * 1000) },
+              { name: "河南", value: Math.round(Math.random() * 1000) },
+              { name: "云南", value: Math.round(Math.random() * 1000) },
+              { name: "辽宁", value: Math.round(Math.random() * 1000) },
+              { name: "黑龙江", value: Math.round(Math.random() * 1000) },
+              { name: "湖南", value: Math.round(Math.random() * 1000) },
+              { name: "安徽", value: Math.round(Math.random() * 1000) },
+              { name: "山东", value: Math.round(Math.random() * 1000) },
+              { name: "新疆", value: Math.round(Math.random() * 1000) },
+              { name: "江苏", value: Math.round(Math.random() * 1000) },
+              { name: "浙江", value: Math.round(Math.random() * 1000) },
+              { name: "江西", value: Math.round(Math.random() * 1000) },
+              { name: "湖北", value: Math.round(Math.random() * 1000) },
+              { name: "广西", value: Math.round(Math.random() * 1000) },
+              { name: "甘肃", value: Math.round(Math.random() * 1000) },
+              { name: "山西", value: Math.round(Math.random() * 1000) },
+              { name: "内蒙古", value: Math.round(Math.random() * 1000) },
+              { name: "陕西", value: Math.round(Math.random() * 1000) },
+              { name: "吉林", value: Math.round(Math.random() * 1000) },
+              { name: "福建", value: Math.round(Math.random() * 1000) },
+              { name: "贵州", value: Math.round(Math.random() * 1000) },
+              { name: "广东", value: Math.round(Math.random() * 1000) },
+              { name: "青海", value: Math.round(Math.random() * 1000) },
+              { name: "西藏", value: Math.round(Math.random() * 1000) },
+              { name: "四川", value: Math.round(Math.random() * 1000) },
+              { name: "宁夏", value: Math.round(Math.random() * 1000) },
+              { name: "海南", value: Math.round(Math.random() * 1000) },
+              { name: "台湾", value: Math.round(Math.random() * 1000) },
+              { name: "香港", value: Math.round(Math.random() * 1000) },
+              { name: "澳门", value: Math.round(Math.random() * 1000) }
+            ]
           }
         ]
       };
