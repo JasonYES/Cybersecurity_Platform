0d9ee98cfcfa395fc35a85ef5c57d8950ac900d7
JasonYES <fyCode@163.com>
2019-03-05 10:50:19 +0800

    map component done

--- src/view/cb-view/charts-test.vue
+++ src/view/cb-view/charts-test.vue
@@ -22,17 +22,17 @@
         </Card>
       </i-col>
     </Row>-->
-    <Row :gutter="20" style="margin-top: 10px;">
+    <!-- <Row :gutter="20" style="margin-top: 10px;">
       <i-col :md="24" :lg="24" style="margin-bottom: 20px;">
         <Card shadow>
           <chart-scatter style="height: 300px;" :value="scatterData" text="显著性统计-国家网络安全策略"></chart-scatter>
         </Card>
       </i-col>
-    </Row>
+    </Row>-->
     <Row :gutter="20" style="margin-top: 10px;">
       <i-col :md="24" :lg="24" style="margin-bottom: 20px;">
         <Card shadow>
-          <chart-map style="height: 300px;" :value="scatterData" text="显著性统计-国家网络安全策略"></chart-map>
+          <chart-map style="height:600px;"></chart-map>
         </Card>
       </i-col>
     </Row>
