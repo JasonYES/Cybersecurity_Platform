c10d727f1ea0cf28a56eeb1c616838cbc2d4b847
JasonYES <fyCode@163.com>
2019-03-10 09:32:11 +0800

    cb-view functionally donw

--- src/components/charts/pie.vue
+++ src/components/charts/pie.vue
@@ -3,68 +3,222 @@
 </template>
 
 <script>
-import echarts from 'echarts'
-import tdTheme from './theme.json'
-import { on, off } from '@/libs/tools'
-echarts.registerTheme('tdTheme', tdTheme)
+import echarts from "echarts";
+import tdTheme from "./theme.json";
+import { on, off } from "@/libs/tools";
+echarts.registerTheme("tdTheme", tdTheme);
 export default {
-  name: 'ChartPie',
+  name: "ChartPie",
   props: {
     value: Array,
     text: String,
     subtext: String
   },
-  data () {
-    return {
-      dom: null
-    }
-  },
   methods: {
-    resize () {
-      this.dom.resize()
+    resize() {
+      this.dom.resize();
+    },
+    countData(data) {
+      for (var i in data) {
+        var index = 1;
+        for (var j in data[i]) {
+          if (j == "country") continue;
+          var index2 = this.countAndDevide(data[i][j]);
+          this.pieData[index][index2] += 1;
+          index++;
+        }
+      }
+      console.log(this.pieData);
+    },
+    countAndDevide(score) {
+      if (score >= 0.8) return 1;
+      else if (score >= 0.6) return 2;
+      else if (score >= 0.4) return 3;
+      else if (score >= 0.2) return 4;
+      else return 5;
     }
   },
-  mounted () {
+  mounted() {
+    this.countData(this.data);
     this.$nextTick(() => {
-      let legend = this.value.map(_ => _.name)
       let option = {
+        dataset: {
+          source: this.pieData
+        },
         title: {
           text: this.text,
           subtext: this.subtext,
-          x: 'center'
+          x: "center"
         },
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-        legend: {
-          orient: 'vertical',
-          left: 'left',
-          data: legend
-        },
+        tooltip: {},
+        legend: {},
         series: [
           {
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '60%'],
-            data: this.value,
-            itemStyle: {
-              emphasis: {
-                shadowBlur: 10,
-                shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
+            type: "pie",
+            seriesLayoutBy: "row",
+            radius: "20%",
+            center: ["33%", "20%"],
+            encode: {
+              itemName: "指标名",
+              value: "指标1"
+            }
+          },
+          {
+            type: "pie",
+            seriesLayoutBy: "row",
+            radius: "20%",
+            center: ["66%", "20%"],
+            encode: {
+              itemName: "指标名",
+              value: "指标2"
+            }
+          },
+          {
+            type: "pie",
+            seriesLayoutBy: "row",
+            radius: "20%",
+            center: ["33%", "50%"],
+            encode: {
+              itemName: "指标名",
+              value: "指标3"
+            }
+          },
+          {
+            type: "pie",
+            seriesLayoutBy: "row",
+            radius: "20%",
+            center: ["66%", "50%"],
+            encode: {
+              itemName: "指标名",
+              value: "指标4"
+            }
+          },
+          {
+            type: "pie",
+            seriesLayoutBy: "row",
+            radius: "20%",
+            center: ["33%", "80%"],
+            encode: {
+              itemName: "指标名",
+              value: "指标5"
+            }
+          },
+          {
+            type: "pie",
+            seriesLayoutBy: "row",
+            radius: "20%",
+            center: ["66%", "80%"],
+            encode: {
+              itemName: "指标名",
+              value: "指标6"
             }
           }
         ]
-      }
-      this.dom = echarts.init(this.$refs.dom, 'tdTheme')
-      this.dom.setOption(option)
-      on(window, 'resize', this.resize)
-    })
+      };
+      this.dom = echarts.init(this.$refs.dom, "tdTheme");
+      this.dom.setOption(option);
+      on(window, "resize", this.resize);
+    });
   },
-  beforeDestroy () {
-    off(window, 'resize', this.resize)
+  data() {
+    return {
+      dom: null,
+      pieData: [
+        ["指标名", "0.81-1.00", "0.61-0.80", "0.41-0.6", "0.21-0.4", "0-0.2"],
+        ["指标1", 0, 0, 0, 0, 0],
+        ["指标2", 0, 0, 0, 0, 0],
+        ["指标3", 0, 0, 0, 0, 0],
+        ["指标4", 0, 0, 0, 0, 0],
+        ["指标5", 0, 0, 0, 0, 0],
+        ["指标6", 0, 0, 0, 0, 0]
+      ],
+      data: [
+        {
+          country: "新加坡",
+          score: 0.92,
+          legal: 0.95,
+          technical: 0.96,
+          organization: 0.88,
+          capacity: 0.97,
+          cooperation: 0.87
+        },
+        {
+          country: "马来西亚",
+          score: 0.55,
+          legal: 0.68,
+          technical: 0.43,
+          organization: 0.55,
+          capacity: 0.28,
+          cooperation: 0.44
+        },
+        {
+          country: "也门",
+          score: 0.22,
+          legal: 0.11,
+          technical: 0.34,
+          organization: 0.25,
+          capacity: 0.44,
+          cooperation: 0.35
+        },
+        {
+          country: "尼日利亚",
+          score: 0.43,
+          legal: 0.99,
+          technical: 0.22,
+          organization: 0.68,
+          capacity: 0.77,
+          cooperation: 0.21
+        },
+        {
+          country: "澳大利亚",
+          score: 0.82,
+          legal: 0.94,
+          technical: 0.96,
+          organization: 0.86,
+          capacity: 0.94,
+          cooperation: 0.44
+        },
+        {
+          country: "日本",
+          score: 0.81,
+          legal: 0.91,
+          technical: 0.77,
+          organization: 0.82,
+          capacity: 0.9,
+          cooperation: 0.7
+        },
+        {
+          country: "法国",
+          score: 0.81,
+          legal: 0.94,
+          technical: 0.96,
+          organization: 0.6,
+          capacity: 1,
+          cooperation: 0.61
+        },
+        {
+          country: "俄罗斯",
+          score: 0.78,
+          legal: 0.82,
+          technical: 0.67,
+          organization: 0.85,
+          capacity: 0.91,
+          cooperation: 0.7
+        },
+        {
+          country: "埃及",
+          score: 0.77,
+          legal: 0.92,
+          technical: 0.92,
+          organization: 0.4,
+          capacity: 0.92,
+          cooperation: 0.7
+        }
+      ]
+    };
+  },
+  beforeDestroy() {
+    off(window, "resize", this.resize);
   }
-}
+};
 </script>
--- src/view/cb-view/significance.vue
+++ src/view/cb-view/significance.vue
@@ -1,17 +1,16 @@
 <template>
   <div>
-    <Row :gutter="20">
-      <i-col :xs="12" :md="8" :lg="4" v-for="(infor, i) in inforCardData" :key="`infor-${i}`" style="height: 120px;padding-bottom: 10px;">
-        <infor-card shadow :color="infor.color" :icon="infor.icon" :icon-size="36">
-          <count-to :end="infor.count" count-class="count-style"/>
-          <p>{{ infor.title }}</p>
-        </infor-card>
+    <Row :gutter="20" style="margin-top: 10px;">
+      <i-col :md="24" :lg="24" style="margin-bottom: 20px;">
+        <Card shadow>
+          <Cbox @checked="checkedData"></Cbox>
+        </Card>
       </i-col>
     </Row>
     <Row :gutter="20" style="margin-top: 10px;">
       <i-col :md="24" :lg="24" style="margin-bottom: 20px;">
         <Card shadow>
-          <chart-pie style="height: 300px;" :value="pieData" text="显著性统计-国家网络安全策略"></chart-pie>
+          <chart-pie style="height: 700px;" :value="pieData" text></chart-pie>
         </Card>
       </i-col>
     </Row>
@@ -19,54 +18,34 @@
 </template>
 
 <script>
-import InforCard from '_c/info-card'
-import CountTo from '_c/count-to'
-import { ChartPie, ChartBar } from '_c/charts'
-import Example from './example.vue'
+import { ChartPie, Cbox } from "_c/charts";
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    InforCard,
-    CountTo,
     ChartPie,
-    ChartBar,
-    Example
+    Cbox
   },
-  data () {
+  methods: {
+    checkedData() {}
+  },
+  data() {
     return {
-      inforCardData: [
-        { title: '国家数', icon: 'md-map', count: 198, color: '#19be6b' },
-        { title: '指标数', icon: 'md-flag', count: 28, color: '#ff9900' },
-        { title: '已爬网页数', icon: 'md-bug', count: 253, color: '#E46CBB' },
-        { title: '数据集数', icon: 'md-cube', count: 3, color: '#ed3f14' },
-        { title: '有效数据条数', icon: 'md-checkmark', count: 1225, color: '#9A66E4' },
-        { title: '总数据条数', icon: 'md-archive', count: 1502, color: '#2d8cf0' },
-      ],
       pieData: [
-        {value: 20, name: '独立制定'},
-        {value: 62, name: '非独立制定'},
-        {value: 30, name: '正在制定'},
-        {value: 70, name: '未制定'}
-      ],
-      barData: {
-        美国: 97,
-        加拿大: 89,
-        墨西哥: 80,
-        古巴: 50,
-        智利: 65,
-        巴西: 78,
-        秘鲁: 30
-      }
-    }
+        { value: 20, name: "独立制定" },
+        { value: 62, name: "非独立制定" },
+        { value: 30, name: "正在制定" },
+        { value: 70, name: "未制定" }
+      ]
+    };
   },
-  mounted () {
+  mounted() {
     //
   }
-}
+};
 </script>
 
 <style lang="less">
-.count-style{
+.count-style {
   font-size: 50px;
 }
 </style>
