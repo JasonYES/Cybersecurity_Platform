4e443fe390eb3aad32dd36d3caa12cfd2a3fcb45
JasonYES <fyCode@163.com>
2019-03-22 13:54:15 +0800

    mark begin

--- src/store/module/cb-data.js
+++ src/store/module/cb-data.js
@@ -1,23 +1,38 @@
+import tmpData from './tmp-data';
 export default {
   state: {
     scores: [],
-    cboxCounties: [],
-    cboxOrgs: [],
-    chosenCountries: [],
-    chosenOrgs: []
+    cbox: {
+      countries: {},
+      orgs: {},
+      chosenCountries: {},
+      chosenOrgs: {}
+    }
   },
   getters: {
-
   },
   mutations: {
     setScores(state, data) {
-      state.scores = data
+      state.scores = data;
+    },
+    setCbox(state, data) {
+      for (var i in data) {
+        state.cbox[i] = data[i];
+      }
     }
   },
   actions: {
     initData(context) {
-      var scores =
-        context.commit('setScores', scores);
+      // 总数据
+      var scores = tmpData["scores"];
+      context.commit('setScores', scores);
+      // cbox
+      var countries = tmpData["countries"];
+      context.commit('setCbox', { "countries": countries });
+      context.commit('setCbox', { "chosenCountries": countries });
+      var orgs = tmpData["orgs"];
+      context.commit('setCbox', { "orgs": orgs });
+      context.commit('setCbox', { "chosenOrgs": orgs });
     }
   }
 }
